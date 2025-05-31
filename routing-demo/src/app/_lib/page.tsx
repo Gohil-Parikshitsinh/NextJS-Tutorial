c33287c097d70290002e6b8ecd036ea843201737
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2025-05-31 18:22:55 +0530

    Tutorial 12: Private Folders

--- routing-demo/src/app/_lib/page.tsx
+++ routing-demo/src/app/_lib/page.tsx
@@ -1,3 +1,12 @@
 export default function PrivaeRoute() {
     return <h1>You cannot view this in the browser</h1>
 }
+
+
+// you can use %5F instead of undeerscore in the _lib name as it also works same as the underscore and still make the folder private
+// Private Folder Contd.
+// Private folder are super userful for a bunch of things:
+// - keeping the UI logic seperated from routing logic
+// - Having a consistent wa to organize internal file in your project
+// - Making it easier to group realted files in your code editor
+// - Avoding potential naming conflicts with future Next.js file naming covenctions 
